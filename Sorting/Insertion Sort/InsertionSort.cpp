b299535dac60bf67cb688e9146aa41011051f8ea
Manasi Patil <36513491+itsmnsi@users.noreply.github.com>
2021-10-18 11:05:23 +0530

    Create InsertionSort.cpp

---
+++ Sorting/Insertion Sort/InsertionSort.cpp
@@ -0,0 +1,23 @@
+***** C++ program *****
+#include<iostream>
+void main()
+{
+ int i,j,k,temp;
+ int a[10] = { 10,9,7,101,23,44,12,78,34,23};
+ cout<<"\n printing sorted elemnts...\n";
+ for(k=1;k<10;k++)
+ {
+  temp=a[k];
+  j=k-1;
+  while(j>=0 && temp<=a[j])
+  {
+   a[j+1]=a[j];
+   j=j-1;
+  }
+  a[j+1]=temp;
+ }
+ for(i=0;i<10;i++)
+ {
+  cout<<a[i]<<"\n";
+ }
+}
