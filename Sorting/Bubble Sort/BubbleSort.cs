ad2cea1bda74bfa7c130a312566d28ca3647ac61
Manasi Patil <36513491+itsmnsi@users.noreply.github.com>
2021-10-18 10:19:12 +0530

    Create BubbleSort.cs

---
+++ Sorting/Bubble Sort/BubbleSort.cs
@@ -0,0 +1,26 @@
+using System;
+public class Program
+{
+ public static void Main()
+ {
+  int i,j,temp;
+  int a[] = {10,9,7,101,23,44,12,78,34,23};
+  for(i=0;i<10;i++)
+  {
+   for(j=i+1;j<10;j++)
+   {
+    if(a[j]>a[i])
+    {
+     temp = a[i];
+     a[i] = a[j];
+     a[j] = temp;
+    }
+   }
+  }
+  Console.WriteLine("printing sorted element list ...\n");
+  for(i=0;i<10;i++)
+  {
+   Console.WriteLine(a[i]);
+  }
+ }
+}
